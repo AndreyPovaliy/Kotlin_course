dc67a60dd65b312d80c35cb725004d50c720b56a
AndreyPovaliy <povandrey16@yandex.ru>
2024-03-24 14:05:29 +0300

    New

--- src/main/kotlin/tasks/task4.kt
+++ src/main/kotlin/tasks/task4.kt
@@ -1,2 +1,29 @@
 package tasks
 
+/*
+Написать функцию, которая для двух целых чисел возвращает
+«первое число больше» если первое больше,
+«второе число больше» если второе больше и «числа равны», если они равны*/
+
+
+fun main() {
+    val number1: Int = readln().toInt()
+    val number2: Int = readln().toInt()
+
+    val maxNumber: String = getStringMaxNumber(number1, number2)
+    println(maxNumber)
+
+}
+
+fun getStringMaxNumber(n1: Int, n2: Int): String {
+    return  if (n1 > n2)
+        "первое число больше"
+    else if (n1 < n2)
+        "второе число больше"
+    else
+        "числа равны"
+
+
+
+
+}
